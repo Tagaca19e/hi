accc7b42811d969250810222cf531e3d1d376e14
Eidmone <eidmonetagaca@gmail.com>
2021-08-10 14:59:51 -0700

    save point

---
+++ .history/src/projectData_20210810064600.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    title: "iSpent | budget tracker  ",
+    image: "./images/iSpent",
+    description:
+      "A voice-activated Expense Tracker App that allows you to track income and expenses across a range of categories, developed to showcase the use of React.JS, Material UI, and the use of libraries.",
+    tools: ["React", "Redux", "MongoDB", "JWT", "Express"],
+    github: "https://github.com/Tagaca19e/myspend",
+    link: "https://tagaca19e.github.io/myspend/",
+  },
+  {
+    title: " GitSearch | search github users",
+    image: "./images/tmdb-mockup",
+    description:
+      "A voice-activated Expense Tracker App that allows you to track income and expenses across a range of categories, developed to showcase the use of React.JS, Material UI, and the use of libraries.",
+    tools: ["React", "API", "SASS"],
+    github: "https://github.com/Deelip7/react-tmdb-app",
+    link: "https://festive-wilson-a51c32.netlify.app/",
+  },
+  {
+    title: "Type-it | typing game ",
+    image: "./images/recipeApp-mockup",
+    description:
+      "A simple and fun game to improve your typing skills, made mainly with Javascript and Css that covers the concepts of creating timers, calling APIs, and hooking up DOM events.",
+    tools: ["Javascript", "API"],
+    github: "https://github.com/Tagaca19e/Type-it",
+    link: "https://tagaca19e.github.io/Type-it/",
+  },
+];
+
+export default projects;
